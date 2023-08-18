a8f3ea2c9af41456f54a5778a4baa7ffd0b7a062
Vansh <77226791+Vansh1190@users.noreply.github.com>
2023-08-18 19:32:01 +0530

    Update chat.js

--- route/chat.js
+++ route/chat.js
@@ -4,10 +4,10 @@ const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config()
 
 router.post('/', async(req, res)=>{
-      if(req.headers.origin != 'http://localhost:8100' &&  req.headers.origin != "https://chatuniverse.vercel.app" && req.headers.origin != 'https://localhost')
+    /*  if(req.headers.origin != 'http://localhost:8100' &&  req.headers.origin != "https://chatuniverse.vercel.app" && req.headers.origin != 'https://localhost')
       {
         return res.status(400).send("Error you don't have access")
-      }
+      }*/
     const configuration = new Configuration({
         organization: "org-5xfr53HrEy5yK3k2PgeruCRT",
         apiKey: process.env.OPENAI_API_KEY,
